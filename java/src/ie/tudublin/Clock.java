84a9811aadbdc0040a3d5960de9b82b22e7758dc
desb-p <C17770271@mydit.ie>
2019-04-18 23:29:26 +0100

    Added a Clock class

---
+++ java/src/ie/tudublin/Clock.java
@@ -0,0 +1,31 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Clock 
+{
+
+    UI ui;
+    float x;
+    float y;
+    float width;
+    float height;
+
+ 
+
+    public Clock(UI ui, float x, float y,float width,float height)
+    {
+        this.ui = ui;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public void render()
+    {
+    
+        
+
+    }
+}
